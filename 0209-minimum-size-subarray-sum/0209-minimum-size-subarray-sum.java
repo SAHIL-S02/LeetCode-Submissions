8478cd383471401f562694bc0f20f74ab55175a3
SK SAHIL UDDIN <sksahilu735@gmail.com>
2026-08-11 19:42:38 +0530

    Time: 1 ms (99.9%), Space: 69.5 MB (10.35%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,7 +1,6 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int re = Integer.MAX_VALUE;
-        int count = 0;
         int sum = 0;
         int low = 0;
         for(int high = 0; high < nums.length; high++){
